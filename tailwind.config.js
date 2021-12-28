427c23944135f7a797f867078c1ac98c581df647
checker <trinhlehainam@protonmail.com>
2021-12-28 12:16:21 +0900

    Add layout

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                "very-dark-magenta": "hsl(300, 43%, 22%)",
+                "soft-pink": "hsl(333, 80%, 67%)",
+                "dark-grayish-magenta": "hsl(303, 10%, 53%)",
+                "light-grayish-magenta": "hsl(300, 24%, 96%)",
+            },
+        },
+        fontFamily: {
+            "Spartan": ["Spartan"],
+        }
+    },
+    plugins: [],
+}
